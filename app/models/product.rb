860e7fbd232eec0bcc129b241b898f99a00188c3
shay <shayofer89@gmail.com>
2014-10-14 13:35:34 +0300

    added featured product and validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+	validates :name, length: { minimum: 2 }
+	validates :description, length: { minimum: 20 }
+	validates :description, length: { maximum: 200 }
 end
